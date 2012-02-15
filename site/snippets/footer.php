c866fadb1379d7a63b4248e2cc5f533a4210fdee
Steve Urmston <steve@designition.co.uk>
2012-02-15 17:02:31 +0000

    Kirby getting there, bit more style and script

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,15 +1,27 @@
-	<footer id="bottom">
-		<dl>
-			<dt>Post:</dt>
-			<dd><?php echo $site->address() ?></dd>
-			<dt>Phone:</dt>
-			<dd><?php echo $site->phone() ?></dd>
-			<dt>Email:</dt>
-			<dd><?php echo $site->email() ?></dd>
-		</dl>
-	</footer>
+		<footer id="bottom">
+			<dl>
+				<dt>Post:</dt>
+				<dd><?php echo $site->address() ?></dd>
+				<dt>Phone:</dt>
+				<dd><?php echo $site->phone() ?></dd>
+				<dt>Email:</dt>
+				<dd><?php echo $site->email() ?></dd>
+			</dl>
+		</footer>
 
-</div>
+	</div>
+		
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="assets/scripts/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+	<script src="assets/scripts/plugins.js"></script>
+	<script src="assets/scripts/script.js"></script>
+	<script>
+		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
 
 </body>
 
--- site/templates/home.php
+++ site/templates/home.php
@@ -19,10 +19,40 @@
 			<?php if($items && $items->count()): ?>
 				
 				<?php foreach($items AS $item): ?>
+					
+					<?php if($section->fragment() == 'portfolio'): ?>
 
-					<article>
-						<h2><?php echo html($item->title()) ?> <em><?php echo html($item->subtitle()) ?></em></h2>
-					</article>
+						<article>
+							
+							<?php $cover = $item->images()->find('cover.jpg') ?>
+							<?php if ($cover) : ?>
+							<figure>
+								<img src="<?php echo $cover->url() ?>" />
+							</figure>
+							<?php endif ?>
+
+							<h2><?php echo html($item->title()) ?> <em><?php echo html($item->subtitle()) ?></em></h2>
+							<?php echo kirbytext($item->text()) ?>
+
+							<?php $panels = $item->children()->visible(); ?>
+							<?php if($panels && $panels->count()): ?>
+								<?php foreach($panels AS $panel): ?>
+									<?php echo kirbytext($panel->text()) ?>
+								<?php endforeach ?>
+							<?php endif; ?>
+
+						</article>
+
+					<?php elseif($section->fragment() == 'services'): ?>
+						
+						<article class="<?php echo $item->fragment() ?>">
+							<h2><?php echo html($item->title()) ?></h2>
+							<?php echo kirbytext($item->text()) ?>
+						</article>
+
+					<?php endif; ?>
+
+					
 
 				<?php endforeach ?>   
 				
