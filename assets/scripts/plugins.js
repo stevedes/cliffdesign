4d72dc4f3d2e6fa22d683b1d8b9d19da98407ad4
Steve Urmston <steve@designition.co.uk>
2012-02-17 13:38:54 +0000

    Basic back button support

--- assets/scripts/plugins.js
+++ assets/scripts/plugins.js
@@ -36,6 +36,17 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 */jQuery.extend({bez:function(a){var b="bez_"+$.makeArray(arguments).join("_").replace(".","p");if(typeof jQuery.easing[b]!="function"){var c=function(a,b){var c=[null,null],d=[null,null],e=[null,null],f=function(f,g){return e[g]=3*a[g],d[g]=3*(b[g]-a[g])-e[g],c[g]=1-e[g]-d[g],f*(e[g]+f*(d[g]+f*c[g]))},g=function(a){return e[0]+a*(2*d[0]+3*c[0]*a)},h=function(a){var b=a,c=0,d;while(++c<14){d=f(b,0)-a;if(Math.abs(d)<.001)break;b-=d/g(b)}return b};return function(a){return f(h(a),1)}};jQuery.easing[b]=function(b,d,e,f,g){return f*c([a[0],a[1]],[a[2],a[3]])(d/g)+e}}return b}});
 
 
+/*
+ * jQuery hashchange event - v1.3 - 7/21/2010
+ * http://benalman.com/projects/jquery-hashchange-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function($,e,b){var c="hashchange",h=document,f,g=$.event.special,i=h.documentMode,d="on"+c in e&&(i===b||i>7);function a(j){j=j||location.href;return"#"+j.replace(/^[^#]*#?(.*)$/,"$1")}$.fn[c]=function(j){return j?this.bind(c,j):this.trigger(c)};$.fn[c].delay=50;g[c]=$.extend(g[c],{setup:function(){if(d){return false}$(f.start)},teardown:function(){if(d){return false}$(f.stop)}});f=(function(){var j={},p,m=a(),k=function(q){return q},l=k,o=k;j.start=function(){p||n()};j.stop=function(){p&&clearTimeout(p);p=b};function n(){var r=a(),q=o(m);if(r!==m){l(m=r,q);$(e).trigger(c)}else{if(q!==m){location.href=location.href.replace(/#.*/,"")+q}}p=setTimeout(n,$.fn[c].delay)}$.browser.msie&&!d&&(function(){var q,r;j.start=function(){if(!q){r=$.fn[c].src;r=r&&r+a();q=$('<iframe tabindex="-1" title="empty"/>').hide().one("load",function(){r||l(a());n()}).attr("src",r||"javascript:0").insertAfter("body")[0].contentWindow;h.onpropertychange=function(){try{if(event.propertyName==="title"){q.document.title=h.title}}catch(s){}}}};j.stop=k;o=function(){return a(q.location.href)};l=function(v,s){var u=q.document,t=$.fn[c].domain;if(v!==s){u.title=h.title;u.open();t&&u.write('<script>document.domain="'+t+'"<\/script>');u.close();q.location.hash=v}}})();return j})()})(jQuery,this);
+
+
 /*!
  * jQuery Transit - CSS3 transitions and transformations
  * Copyright(c) 2011 Rico Sta. Cruz <rico@ricostacruz.com>
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -25,10 +25,40 @@
 		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
 	};
 
+	var cliffScroll = function(e) {
+		
+		var target_top = $(e).offset().top;
+		var current_position = $(window).scrollTop();
+		var distance = 0;
+
+		// going back up
+		if (current_position > target_top) {
+			distance = current_position - target_top;
+		}
+		// going down
+		else {
+			distance = target_top - current_position;
+		}
+
+		var time_to_scroll = 300 + ((distance / 1000) * 300);
+
+		$('html, body').animate(
+			{ scrollTop: target_top },
+			time_to_scroll,
+			$.bez([.59,.17,.39,1.19]),
+			function() {
+			
+				window.location.hash = e;
+			}
+		);
+
+	}
+
 
     /*
     	Custom Plugins
     */
+
 
     /*
     	Make Rocket Go Now!
@@ -45,10 +75,26 @@
 
 		body.find('#scrollfix').height($(window).height());
 
-		body.find('#navigation a').click(function() {
+		body.find('#navigation a').click(function(e) {
 			
-			var target = $(this).attr('href');
-			var target_top = $(target).offset().top;
+			e.preventDefault();
+
+			var targ = $(this).attr('href');
+
+			//window.location.hash = targ;
+
+			cliffScroll(targ);
+
+			//return false;
+		});
+		
+		$(window).hashchange( function(event){
+
+			event.preventDefault();
+
+			/*
+		
+			var target_top = $(location.hash).offset().top;
 			var current_position = $(window).scrollTop();
 			var distance = 0;
 
@@ -60,22 +106,20 @@
 			else {
 				distance = target_top - current_position;
 			}
-			//alert($(window).scrollTop());
 
-			//$('html, body').scrollTo(target, 650, {easing: $.bez([.75,.36,.35,1.16])});
+			var time_to_scroll = 300 + ((distance / 1000) * 300);
 
-			var time_to_scroll = 200 + ((distance / 1000) * 400);
+			$('html, body').animate(
+				{ scrollTop: 29 },
+				500
+			);
 
-			//alert(time_to_scroll);
-
-			$('html, body').animate({
-			    scrollTop: target_top
-			}, time_to_scroll, $.bez([.59,.04,.35,1.16]));
-			
-			return false;
+			*/
+			cliffScroll(location.hash);
+	
 		});
-		
-
+				
+	
 		
 
 	});
